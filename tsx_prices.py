3cb6108b52e4cfcceceadd89d1b4bdbd6f71f226
Brian J Dixon <bjdixon@gmail.com>
2014-07-23 23:10:18 -0400

    small refactor for legibility and improvement of comments

--- tsx_prices.py
+++ tsx_prices.py
@@ -8,33 +8,30 @@ def error(f, x, y):
 def add_model_to_chart(x, y, degree):
     # x values for plotting
     fx = sp.linspace(0, x[-1], 1000)
-    # model (polynomial of degree...
+    # model (polynomial of degree...)
     fp = sp.polyfit(x, y, degree)
     f = sp.poly1d(fp)
     plt.plot(fx, f(fx), linewidth=3, label="d=%d" % degree)
     return f
 
-# get data from csv file
+# take data from csv file and add it to the chart
 data = sp.genfromtxt("tsxclose.csv", delimiter=",")
 x = np.array(range(1, data.shape[0]+1))
 y = data[::-1, 1] # data is in reverse chronological order, so reverse again
 plt.scatter(x, y)
-
-# add labels
-plt.title("TSX daily close from July 29, 1979 to July 21, 2014")
-plt.xlabel("Day number")
-plt.ylabel("Closing price")
 
 # create models
 for d in (3, 6, 9):
     f = add_model_to_chart(x, y, d)
     print("Model %d error %d" % (d, error(f, x, y)))
 
-# legend
+# make the chart pretty with labels, title and a legend
+plt.title("TSX daily close from July 29, 1979 to July 21, 2014")
+plt.xlabel("Day number")
+plt.ylabel("Closing price")
 plt.legend(loc="upper left")
 
 # print to screen
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
-
